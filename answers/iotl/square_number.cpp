cda0c758f1a80014a3466996ccd2f57e0f09f945
AlbatrossC <soham_jadhav_aids@moderncoe.edu.in>
2025-02-06 00:30:51 +0530

    Wdfervg

--- answers/iotl/square_number.cpp
+++ answers/iotl/square_number.cpp
@@ -1,30 +1,22 @@
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Input a number:");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  // Wait until there's some input in the serial buffer
   if (Serial.available() > 0) {
-    int input = Serial.parseInt(); // Read the input number from the serial monitor
-    
-    // Check if a valid number was entered
+    int input = Serial.parseInt();
     if (input != 0) {
-      int inputSquared = square(input);  // Use the new square function
+      int inputSquared = square(input);
       Serial.print("Squared: ");
-      Serial.println(inputSquared);    // Output the squared value
+      Serial.println(inputSquared);
     } else {
       Serial.println("Please enter a valid number.");
     }
   }
-
-  delay(500);  // Wait for 500ms before checking again
+  delay(500);
 }
 
-// Define a new function to square the input number
 int square(int num) {
   return num * num;
 }

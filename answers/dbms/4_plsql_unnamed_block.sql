d5c282a8a7d2cc784a4e4af258ba7007bea9cd8f
Soham <pes.soham135@gmail.com>
2025-11-08 00:30:15 +0530

    Changes

--- answers/dbms/4_plsql_unnamed_block.sql
+++ answers/dbms/4_plsql_unnamed_block.sql
@@ -33,50 +33,29 @@ SELECT * FROM borrower;
 
 -- 5. Create stored procedure
 DELIMITER //
-
 CREATE PROCEDURE fine_check(IN p_roll_no INT, IN p_book VARCHAR(100))
 BEGIN
     DECLARE v_days, v_fine INT DEFAULT 0;
-
-    -- Calculate number of days since issue
-    SELECT DATEDIFF(CURDATE(), date_of_issue)
-    INTO v_days
+    
+    SELECT DATEDIFF(CURDATE(), date_of_issue) INTO v_days
     FROM borrower
     WHERE roll_no = p_roll_no AND name_of_book = p_book;
-
-    -- Handle missing record
-    IF v_days IS NULL THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'No such borrower or book found';
-    ELSE
-        -- Fine calculation
-        SET v_fine = 
-            CASE 
-                WHEN v_days > 30 THEN v_days * 50
-                WHEN v_days >= 15 THEN v_days * 5
-                ELSE 0
-            END;
-
-        -- Update borrower status to Returned
-        UPDATE borrower
-        SET status = 'R'
-        WHERE roll_no = p_roll_no AND name_of_book = p_book;
-
-        -- Insert fine if applicable
-        IF v_fine > 0 THEN
-            INSERT INTO fine VALUES (p_roll_no, CURDATE(), v_fine);
-        END IF;
-
-        -- Display result message
-        SELECT CONCAT('Book returned successfully. Days kept: ', v_days,
-                      ', Fine: Rs ', v_fine) AS Result;
+    
+    IF v_days > 30 THEN SET v_fine = v_days * 50;
+    ELSEIF v_days >= 15 THEN SET v_fine = v_days * 5;
     END IF;
-END;
-//
+    
+    UPDATE borrower SET status = 'R'
+    WHERE roll_no = p_roll_no AND name_of_book = p_book;
+    
+    IF v_fine > 0 THEN
+        INSERT INTO fine VALUES (p_roll_no, CURDATE(), v_fine);
+    END IF;
+END //
 DELIMITER ;
 
 -- 6. Execute the procedure
 CALL fine_check(6, 'AI');
 
--- 7. View fine table
+-- 7. View results
 SELECT * FROM fine;
